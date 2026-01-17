e4906d9f6b1bd2bc53897b243a8c4ed1b0183f79
shakanver <shakeelanver26@gmail.com>
2026-01-17 18:15:49 +1100

    added new no ros api to just focus on motors, remote control api with ros is wip

--- ariesmk1/remote_control_api_noros.py
+++ ariesmk1/remote_control_api_noros.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3
-import rclpy
-from rclpy.executors import ExternalShutdownException
-from rclpy.node import Node
-from cv_bridge import CvBridge
-import cv2
-from sensor_msgs.msg import Image
 from flask import Flask, render_template, Response, request, jsonify, redirect, url_for, make_response
 import cv2
 import logging #TODO: use logging lib to make better logs? 
@@ -23,22 +17,9 @@ THROTTLE_INCREMENT = 5
 logging.basicConfig(format='%(asctime)s %(message)s')
 logger = logging.getLogger()
 
-class RemoteControlApi(Node):
+class RemoteControlApi():
 
 	def __init__(self):
-		super().__init__('remote_control_api')
-		'''Create a subscriber that will receive frames from the ros pi camera node.'''
-		self.subscription = self.create_subscription(
-			Image,
-			'/camera/image_raw',
-			self._camera_img_callback,
-			10)
-		self.subscription 
-
-		self.bridge = CvBridge()
-		self.prev_time = self.get_clock().now()
-		self.curr_frame = None
-		self.lock_curr_frame = threading.Lock()
 
 		'''Create a Python Flask app that will provide a livestream from the raspberry pi camera and except rc controls from the user'''
 		templates_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
@@ -46,41 +27,11 @@ class RemoteControlApi(Node):
 		self._register_routes()
 
 		'''Initialize connection to flight controller to send command via mavlink protocol '''
+		# TODO: make this not stall the flask app if no connection
 		self.mavlink_connection = mavutil.mavlink_connection('/dev/ttyAMA0', baud=921600)
 		self.mavlink_connection.wait_heartbeat()
 		logger.info("Hearbeat received from flight controller. Mavlink connection successful.")
 
-	'''
-	ROS2 Methods
-	'''
-	def _camera_img_callback(self, msg):
-        # Compute FPS
-		curr_time = self.get_clock().now()
-		dt = (curr_time - self.prev_time).nanoseconds / 1e9
-		fps = 1.0 / dt if dt > 0 else 0.0
-		self.prev_time = curr_time
-
-		'''
-		uncomment for debug logs #TODO: find a cleaner way to toggle this log
-		'''
-		# self.get_logger().info(f"Received image: height={msg.height}, width={msg.width}")
-		# self.get_logger().info(f"FPS: {fps:.2f}")
-
-		try:
-			# Convert ROS image to OpenCV
-			frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
-			'''
-			uncomment for debug logs #TODO: find a cleaner way to toggle this log
-			'''
-			self.get_logger().info(f"Frame received: {frame.shape}, FPS ~{fps:.2f}")
-			# Flip image (180° rotation)
-			rotated = cv2.flip(frame, -1)
-			with self.lock_curr_frame:
-				self.curr_frame = rotated	
-
-		except Exception as e:
-			self.get_logger().error(f"Error processing image: {e}")
-	
 	'''
 	Flask API Methods
 	'''
@@ -173,16 +124,8 @@ class RemoteControlApi(Node):
 
 
 def main(args=None):
-	with rclpy.init(args=args):
-		remote_controller_api = RemoteControlApi()
-		flask_thread = threading.Thread(target=remote_controller_api.run_flask_app)
-		try:
-				flask_thread.start()
-				rclpy.spin(remote_controller_api)
-		except (KeyboardInterrupt, ExternalShutdownException):
-			pass
-		finally:
-			flask_thread.join()
+	remote_controller_api = RemoteControlApi()
+	remote_controller_api.run_flask_app()
 
 if __name__ == '__main__':
     main()
